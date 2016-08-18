d3ab2ddd66da53e90b0789362704de517b1df652
Phil Holden <phil.m.holden@gmail.com>
2016-08-18 10:06:51 +0100

    flow tabs

--- src/components/app.js
+++ src/components/app.js
@@ -1,35 +1,12 @@
 // @flow
 
 import React from 'react'
-import Fetcher from './fetcher'
-import { getGreeting } from '../api'
-import man from '../../img/man.png'
-import MultiselectDropdown from './multiselect-dropdown'
 import Tabs from './tabs'
-//import { observer } from 'mobx-react'
-
-export const HelloWorld = () => <div>Hello World.</div>
-export const NotUsed = () => <div>Not Used.</div>
 
 function App() {
   return (
     <div className="container">
-      <h1>User Report</h1>
-      <div className="di-container first">
-        <h1>Todos</h1>
-        <MultiselectDropdown
-          defaultValue={['promise']}
-          options={[
-            {
-              label: 'promise',
-              value: 'promise',
-            },
-            {
-              label: 'town',
-              value: 'town',
-            },
-          ]}
-        />
+
         <Tabs
           panes={[
             {
@@ -43,16 +20,7 @@ function App() {
             },
           ]}
         />
-        <div className="row">
-          <div className="col-sm-12">
-            <div>
-              <HelloWorld />
-              <img src={man} alt="man" />
-              <Fetcher getGreeting={getGreeting} />
-            </div>
-          </div>
-        </div>
-      </div>
+
     </div>
   )
 }
--- src/hocs/add-style.js
+++
@@ -1,6 +0,0 @@
-import { mapProps } from 'recompose'
-
-export const addStyle = styles => mapProps(props => ({
-  ...props,
-  style: [styles, props.style],
-}))
